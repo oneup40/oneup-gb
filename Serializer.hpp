704718f42b057f5278523a0d0f59ac36ffc6c840
oneup <oneup@arch64-vm>
2016-12-19 20:31:43 -0500

    cleanup Serializer

--- Serializer.hpp
+++ Serializer.hpp
@@ -31,289 +31,286 @@
 
 namespace gblr {
 
-// forward declarations
-
-class Serializer;
-class Deserializer;
-
-static inline Serializer& operator<<(Serializer &s, bool val);
-static inline Serializer& operator<<(Serializer &s, u8 val);
-static inline Serializer& operator<<(Serializer &s, u16 val);
-static inline Serializer& operator<<(Serializer &s, u32 val);
-static inline Serializer& operator<<(Serializer &s, u64 val);
-static inline Serializer& operator<<(Serializer &s, i8 val);
-static inline Serializer& operator<<(Serializer &s, i16 val);
-static inline Serializer& operator<<(Serializer &s, i32 val);
-static inline Serializer& operator<<(Serializer &s, i64 val);
-static inline Serializer& operator<<(Serializer &s, float val);
-static inline Serializer& operator<<(Serializer &s, double val);
-template <typename T> static inline Serializer& operator<<(Serializer &s, const std::basic_string<T> &val);
-template <typename T> static inline Serializer& operator<<(Serializer &s, const std::vector<T> &val);
-template <typename T, size_t N> static inline Serializer& operator<<(Serializer &s, const std::array<T, N> &val);
-
-static inline Deserializer& operator>>(Deserializer &d, bool &val);
-static inline Deserializer& operator>>(Deserializer &d, u8 &val);
-static inline Deserializer& operator>>(Deserializer &d, u16 &val);
-static inline Deserializer& operator>>(Deserializer &d, u32 &val);
-static inline Deserializer& operator>>(Deserializer &d, u64 &val);
-static inline Deserializer& operator>>(Deserializer &d, i8 &val);
-static inline Deserializer& operator>>(Deserializer &d, i16 &val);
-static inline Deserializer& operator>>(Deserializer &d, i32 &val);
-static inline Deserializer& operator>>(Deserializer &d, i64 &val);
-static inline Deserializer& operator>>(Deserializer &d, float &val);
-static inline Deserializer& operator>>(Deserializer &d, double &val);
-template <typename T> static inline Deserializer& operator>>(Deserializer &d, std::basic_string<T> &val);
-template <typename T> static inline Deserializer& operator>>(Deserializer &d, std::vector<T> &val);
-template <typename T, size_t N> static inline Deserializer& operator>>(Deserializer &d, std::array<T, N> &val);
-
 class Serializer {
 	std::ostream *os_;
 
-	template <typename T>
-	void DoWrite(const T& val) { os_->write(reinterpret_cast<const char*>(&val), sizeof(val)); }
+	template <typename T> void DoWrite(const T& val);
+
+	Serializer& SerializeBool(bool val);
+	template <typename T> Serializer& SerializeUint(T val);
+	template <typename T> Serializer& SerializeInt(T val);
+	template <typename T> Serializer& SerializeFloat(T val);
+	template <typename T> Serializer& SerializeString(const T& val);
+	template <typename T> Serializer& SerializeContainer(const T& val);
+	template <typename T> Serializer& SerializeArray(const T& val);
 public:
-	explicit Serializer(std::ostream *os) : os_(os) {}
+	explicit Serializer(std::ostream *os);
 	Serializer(const Serializer&) = delete;
 	Serializer(Serializer&&) = delete;
 	Serializer& operator=(const Serializer&) = delete;
 	Serializer& operator=(Serializer&&) = delete;
 
-	explicit operator bool() { return static_cast<bool>(*os_); }
+	explicit operator bool();
+	void Fail();
 
-	void Fail() { os_->setstate(std::ios_base::failbit); }
+	Serializer& Start(u64 code);
 
-	Serializer& SerializeBool(bool val) {
-		u64 raw = htole<u64>(val);
-		DoWrite(raw);
+	Serializer& operator<<(bool val);
 
-		return *this;
-	}
+	Serializer& operator<<(u8 val);
+	Serializer& operator<<(u16 val);
+	Serializer& operator<<(u32 val);
+	Serializer& operator<<(u64 val);
 
-	template <typename T>
-	Serializer& SerializeUint(T val) {
-		static_assert(sizeof(u64) == 8, "Need 8-byte u64");
-		u64 raw = htole<u64>(val);
-		DoWrite(raw);
+	Serializer& operator<<(i8 val);
+	Serializer& operator<<(i16 val);
+	Serializer& operator<<(i32 val);
+	Serializer& operator<<(i64 val);
 
-		return *this;
-	}
-
-	template <typename T>
-	Serializer& SerializeInt(T val) {
-		static_assert(sizeof(i64) == 8, "Need 8-byte i64");
-		i64 raw = htole<i64>(val);
-		DoWrite(raw);
-
-		return *this;
-	}
+	Serializer& operator<<(float val);
+	Serializer& operator<<(double val);
 
 	template <typename T>
-	Serializer& SerializeFloat(T val) {
-		static_assert(sizeof(double) == 8, "Need 8-byte double");
-		static_assert(std::numeric_limits<double>::is_iec559, "Need IEEE 754 double");
-		DoWrite<double>(val);
-
-		return *this;
-	}
+	Serializer& operator<<(const std::basic_string<T> &val);
 
 	template <typename T>
-	Serializer& SerializeString(const T& val) {
-		(*this) << val.size();
+	Serializer& operator<<(const std::vector<T> &val);
 
-		size_t n = 0;
-		for (auto ch : val) {
-			n += sizeof(ch);
-			DoWrite(ch);
-		}
-
-		while (n & 7) {
-			n += sizeof(char);
-			DoWrite<char>(0);
-		}
-
-		return *this;
-	}
-
-	template <typename T>
-	Serializer& SerializeContainer(const T& val) {
-		(*this) << val.size();
-
-		for (const auto &item : val) {
-			(*this) << item;
-		}
-
-		return *this;
-	}
-
-	template <typename T>
-	Serializer& SerializeArray(const T& val) {
-		for (const auto &item : val) {
-			(*this) << item;
-		}
-
-		return *this;
-	}
-
-	Serializer& Start(u64 code) { return (*this) << code; }
+	template <typename T, size_t N>
+	Serializer& operator<<(const std::array<T, N> &val);
 };
 
 class Deserializer {
 	std::istream *is_;
 
 	template <typename T>
-	void DoRead(T *val) { is_->read(reinterpret_cast<char*>(val), sizeof(*val)); }
+	void DoRead(T *val);
+
+	Deserializer& DeserializeBool(bool *val);
+	template <typename T> Deserializer& DeserializeUint(T *val);
+	template <typename T> Deserializer& DeserializeInt(T *val);
+	template <typename T> Deserializer& DeserializeFloat(T *val);
+	template <typename T> Deserializer& DeserializeString(T *val);
+	template <typename T> Deserializer& DeserializeContainer(T *val);
+	template <typename T> Deserializer& DeserializeArray(T* val);
 public:
-	explicit Deserializer(std::istream *is) : is_(is) {}
+	explicit Deserializer(std::istream *is);
 	Deserializer(const Deserializer&) = delete;
 	Deserializer(Deserializer&&) = delete;
 	Deserializer& operator=(const Deserializer&) = delete;
 	Deserializer& operator=(Deserializer&&) = delete;
 
-	explicit operator bool() { return static_cast<bool>(*is_); }
+	explicit operator bool();
+	void Fail();
 
-	void Fail() { is_->setstate(std::ios_base::failbit); }
+	Deserializer& Start(u64 code);
 
-	Deserializer& DeserializeBool(bool *val) {
-		u64 raw = 0;
-		DoRead(&raw);
-		*val = letoh(raw);
+	Deserializer& operator>>(bool &val);
 
-		return *this;
-	}
+	Deserializer& operator>>(u8 &val);
+	Deserializer& operator>>(u16 &val);
+	Deserializer& operator>>(u32 &val);
+	Deserializer& operator>>(u64 &val);
 
-	template <typename T>
-	Deserializer& DeserializeUint(T *val) {
-		static_assert(sizeof(u64) == 8, "Need 8-byte u64");
+	Deserializer& operator>>(i8 &val);
+	Deserializer& operator>>(i16 &val);
+	Deserializer& operator>>(i32 &val);
+	Deserializer& operator>>(i64 &val);
 
-		u64 raw = 0;
-		DoRead(&raw);
-		*val = letoh(raw);
-
-		return *this;
-	}
+	Deserializer& operator>>(float &val);
+	Deserializer& operator>>(double &val);
 
 	template <typename T>
-	Deserializer& DeserializeInt(T *val) {
-		static_assert(sizeof(i64) == 8, "Need 8-byte i64");
-
-		i64 raw = 0;
-		DoRead(&raw);
-		*val = letoh(raw);
-
-		return *this;
-	}
+	Deserializer& operator>>(std::basic_string<T> &val);
 
 	template <typename T>
-	Deserializer& DeserializeFloat(T *val) {
-		static_assert(sizeof(double) == 8, "Need 8-byte double");
-		static_assert(std::numeric_limits<double>::is_iec559, "Need IEEE 754 double");
+	Deserializer& operator>>(std::vector<T> &val);
 
-		double raw = 0;
-		DoRead(&raw);
-		*val = raw;
-
-		return *this;
-	}
-
-	template <typename T>
-	Deserializer& DeserializeString(T *val) {
-		u64 n = 0;
-		(*this) >> n;
-		if (!*this) { return *this; }
-
-		val->resize(n / sizeof(typename T::value_type));
-		for (auto &ch : *val) {
-			DoRead(&ch);
-		}
-
-		char ch = 0;
-		while (n & 7) {
-			n += sizeof(char);
-			DoRead(&ch);
-		}
-
-		return *this;
-	}
-
-	template <typename T>
-	Deserializer& DeserializeContainer(T *val) {
-		u64 n;
-		(*this) >> n;
-		if (!*this) { return *this; }
-
-		val->resize(n);
-		for (auto &item : *val) {
-			(*this) >> item;
-		}
-
-		return *this;
-	}
-
-	template <typename T>
-	Deserializer& DeserializeArray(T* val) {
-		for (auto &item : *val) {
-			(*this) >> item;
-		}
-
-		return *this;
-	}
-
-	Deserializer& Start(u64 code) {
-		u64 extracted = 0;
-		(*this) >> extracted;
-		if (!*this) { return *this; }
-
-		if (extracted != code) { Fail(); }
-		return *this;
-	}
+	template <typename T, size_t N>
+	Deserializer& operator>>(std::array<T, N> &val);
 };
 
-static inline Serializer& operator<<(Serializer &s, bool val)   { return s.SerializeBool(val); }
-
-static inline Serializer& operator<<(Serializer &s, u8 val) 	{ return s.SerializeUint(val); }
-static inline Serializer& operator<<(Serializer &s, u16 val) 	{ return s.SerializeUint(val); }
-static inline Serializer& operator<<(Serializer &s, u32 val) 	{ return s.SerializeUint(val); }
-static inline Serializer& operator<<(Serializer &s, u64 val) 	{ return s.SerializeUint(val); }
-
-static inline Serializer& operator<<(Serializer &s, i8 val) 	{ return s.SerializeInt(val); }
-static inline Serializer& operator<<(Serializer &s, i16 val) 	{ return s.SerializeInt(val); }
-static inline Serializer& operator<<(Serializer &s, i32 val) 	{ return s.SerializeInt(val); }
-static inline Serializer& operator<<(Serializer &s, i64 val) 	{ return s.SerializeInt(val); }
-
-static inline Serializer& operator<<(Serializer &s, float val)	{ return s.SerializeFloat(val); }
-static inline Serializer& operator<<(Serializer &s, double val)	{ return s.SerializeFloat(val); }
+template <typename T>
+void Serializer::DoWrite(const T& val) {
+	os_->write(reinterpret_cast<const char*>(&val), sizeof(val));
+}
 
 template <typename T>
-static inline Serializer& operator<<(Serializer &s, const std::basic_string<T> &val)	{ return s.SerializeString(val); }
+Serializer& Serializer::SerializeUint(T val) {
+	static_assert(sizeof(u64) == 8, "Need 8-byte u64");
+	u64 raw = htole<u64>(val);
+	DoWrite(raw);
+
+	return *this;
+}
 
 template <typename T>
-static inline Serializer& operator<<(Serializer &s, const std::vector<T> &val)		{ return s.SerializeContainer(val); }
+Serializer& Serializer::SerializeInt(T val) {
+	static_assert(sizeof(i64) == 8, "Need 8-byte i64");
+	i64 raw = htole<i64>(val);
+	DoWrite(raw);
+
+	return *this;
+}
+
+template <typename T>
+Serializer& Serializer::SerializeFloat(T val) {
+	static_assert(sizeof(double) == 8, "Need 8-byte double");
+	static_assert(std::numeric_limits<double>::is_iec559, "Need IEEE 754 double");
+	DoWrite<double>(val);
+
+	return *this;
+}
+
+template <typename T>
+Serializer& Serializer::SerializeString(const T& val) {
+	(*this) << val.size();
+
+	size_t n = 0;
+	for (auto ch : val) {
+		n += sizeof(ch);
+		DoWrite(ch);
+	}
+
+	while (n & 7) {
+		n += sizeof(char);
+		DoWrite<char>(0);
+	}
+
+	return *this;
+}
+
+template <typename T>
+Serializer& Serializer::SerializeContainer(const T& val) {
+	(*this) << val.size();
+
+	for (const auto &item : val) {
+		(*this) << item;
+	}
+
+	return *this;
+}
+
+template <typename T>
+Serializer& Serializer::SerializeArray(const T& val) {
+	for (const auto &item : val) {
+		(*this) << item;
+	}
+
+	return *this;
+}
+
+template <typename T>
+Serializer& Serializer::operator<<(const std::basic_string<T> &val) {
+	return SerializeString(val);
+}
+
+template <typename T>
+Serializer& Serializer::operator<<(const std::vector<T> &val) {
+	return SerializeContainer(val);
+}
 
 template <typename T, size_t N>
-static inline Serializer& operator<<(Serializer &s, const std::array<T, N> &val)	{ return s.SerializeArray(val); }
-
-static inline Deserializer& operator>>(Deserializer &d, bool &val)   { return d.DeserializeBool(&val); }
-
-static inline Deserializer& operator>>(Deserializer &d, u8 &val) 	{ return d.DeserializeUint(&val); }
-static inline Deserializer& operator>>(Deserializer &d, u16 &val) { return d.DeserializeUint(&val); }
-static inline Deserializer& operator>>(Deserializer &d, u32 &val) { return d.DeserializeUint(&val); }
-static inline Deserializer& operator>>(Deserializer &d, u64 &val) { return d.DeserializeUint(&val); }
-
-static inline Deserializer& operator>>(Deserializer &d, i8 &val) 	{ return d.DeserializeInt(&val); }
-static inline Deserializer& operator>>(Deserializer &d, i16 &val) { return d.DeserializeInt(&val); }
-static inline Deserializer& operator>>(Deserializer &d, i32 &val) { return d.DeserializeInt(&val); }
-static inline Deserializer& operator>>(Deserializer &d, i64 &val) { return d.DeserializeInt(&val); }
-
-static inline Deserializer& operator>>(Deserializer &d, float &val)		{ return d.DeserializeFloat(&val); }
-static inline Deserializer& operator>>(Deserializer &d, double &val)	{ return d.DeserializeFloat(&val); }
+Serializer& Serializer::operator<<(const std::array<T, N> &val) {
+	return SerializeArray(val);
+}
 
 template <typename T>
-static inline Deserializer& operator>>(Deserializer &d, std::basic_string<T> &val)	{ return d.DeserializeString(&val); }
+void Deserializer::DoRead(T *val) {
+	is_->read(reinterpret_cast<char*>(val), sizeof(*val));
+}
 
 template <typename T>
-static inline Deserializer& operator>>(Deserializer &d, std::vector<T> &val)	{ return d.DeserializeContainer(&val); }
+Deserializer& Deserializer::DeserializeUint(T *val) {
+	static_assert(sizeof(u64) == 8, "Need 8-byte u64");
+
+	u64 raw = 0;
+	DoRead(&raw);
+	*val = letoh(raw);
+
+	return *this;
+}
+
+template <typename T>
+Deserializer& Deserializer::DeserializeInt(T *val) {
+	static_assert(sizeof(i64) == 8, "Need 8-byte i64");
+
+	i64 raw = 0;
+	DoRead(&raw);
+	*val = letoh(raw);
+
+	return *this;
+}
+
+template <typename T>
+Deserializer& Deserializer::DeserializeFloat(T *val) {
+	static_assert(sizeof(double) == 8, "Need 8-byte double");
+	static_assert(std::numeric_limits<double>::is_iec559, "Need IEEE 754 double");
+
+	double raw = 0;
+	DoRead(&raw);
+	*val = raw;
+
+	return *this;
+}
+
+template <typename T>
+Deserializer& Deserializer::DeserializeString(T *val) {
+	u64 n = 0;
+	(*this) >> n;
+	if (!*this) { return *this; }
+
+	val->resize(n / sizeof(typename T::value_type));
+	for (auto &ch : *val) {
+		DoRead(&ch);
+	}
+
+	char ch = 0;
+	while (n & 7) {
+		n += sizeof(char);
+		DoRead(&ch);
+	}
+
+	return *this;
+}
+
+template <typename T>
+Deserializer& Deserializer::DeserializeContainer(T *val) {
+	u64 n;
+	(*this) >> n;
+	if (!*this) { return *this; }
+
+	val->resize(n);
+	for (auto &item : *val) {
+		(*this) >> item;
+	}
+
+	return *this;
+}
+
+template <typename T>
+Deserializer& Deserializer::DeserializeArray(T* val) {
+	for (auto &item : *val) {
+		(*this) >> item;
+	}
+
+	return *this;
+}
+
+template <typename T>
+Deserializer& Deserializer::operator>>(std::basic_string<T> &val)	{
+	return DeserializeString(&val);
+}
+
+template <typename T>
+Deserializer& Deserializer::operator>>(std::vector<T> &val) {
+	return DeserializeContainer(&val);
+}
 
 template <typename T, size_t N>
-static inline Deserializer& operator>>(Deserializer &d, std::array<T, N> &val)	{ return d.DeserializeArray(&val); }
+Deserializer& Deserializer::operator>>(std::array<T, N> &val)	{
+	return DeserializeArray(&val);
+}
+
+
 }	// namespace gblr
-
