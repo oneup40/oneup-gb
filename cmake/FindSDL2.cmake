b3088e816228a530801bd3c55ede88f671da2bbf
oneup <oneup@arch64-vm>
2017-02-11 16:58:41 -0500

    [nit] Remove trailing whitespace

--- cmake/FindSDL2.cmake
+++ cmake/FindSDL2.cmake
@@ -58,7 +58,7 @@
 # Distributed under the OSI-approved BSD License (the "License");
 # see accompanying file Copyright.txt for details.
 #
-# This software is distributed WITHOUT ANY WARRANTY; without even the 
+# This software is distributed WITHOUT ANY WARRANTY; without even the
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the License for more information.
 #=============================================================================
