e881e917c5b9b55da5483399cf1b3684d1b0086b
oneup40 <oneup40@gmail.com>
2017-02-18 17:40:06 -0500

    [cpu] Add CPUObserver

---
+++ core/cpu/CPUObserver.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+// Copyright 2017 oneup
+
+#include "core/Base.hpp"
+#include "core/cpu/Opcode.hpp"
+#include "core/cpu/Operand.hpp"
+#include "core/cpu/Optype.hpp"
+
+namespace gb1 {
+
+class CPU;
+
+class CPUObserver {
+public:
+    virtual void StartTick(const CPU& /* cpu */) {}
+    virtual void FetchInstruction(const CPU& /* cpu */, u16 /* pc */, u8* /* bytes */, size_t /* nbytes */) {}
+    virtual void Decode(const CPU& /* cpu */, const Opcode& /* op */) {}
+    virtual void FetchSourceOperand(const CPU& /* cpu */, const Operand& /* op */) {}
+    virtual void FetchDestinationOperand(const CPU& /* cpu */, const Operand& /* op */) {}
+    virtual void BeforeExecute(const CPU& /* cpu */, Optype /* op */, const Operand& /* src */, const Operand& /* dst */) {}
+    virtual void AfterExecute(const CPU& /* cpu */, Optype /* op */, const Operand& /* src */, const Operand& /* dst */) {}
+    virtual void Store(const CPU& /* cpu */, const Operand& /* dst */) {}
+
+    virtual void RaiseInterrupt(const CPU& /* cpu */, u8 /* num */) {}
+    virtual void ServiceInterrupt(const CPU& /* cpu */, u8 /* num */) {}
+
+    virtual ~CPUObserver() {}
+};
+
+}   // namespace gb1
